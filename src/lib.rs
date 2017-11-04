e393cb1c81ba8272be15753411ff53d41df8a780
Dimitri Merejkowsky <d.merej@gmail.com>
2017-11-04 19:11:52 +0100

    Can copy one file to an other
    Perfs are already *very* close to `cp` :=)

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+extern crate tempdir;
+
+const BUFFER_SIZE: usize = 100 * 1024;
+
+use std::fs::File;
+use std::io::{self, Write};
+use std::io::BufReader;
+use std::io::BufWriter;
+use std::io::prelude::*;
+
 #[derive(Debug)]
 struct TransferRequest<'a> {
     sources: Vec<&'a str>,
@@ -19,21 +29,46 @@ impl<'a> TransferRequest<'a> {
     }
 }
 
-fn do_transfer(request: &TransferRequest) -> Result<(), String> {
+fn copy(source: &str, destination: &str) -> io::Result<()> {
+    println!("{} -> {}", source, destination);
+    let src_path = File::open(source)?;
+    let mut buf_reader = BufReader::new(src_path);
+    let dest_path = File::create(destination)?;
+    let mut buf_writer = BufWriter::new(dest_path);
+    let mut total = 0;
+    let mut buffer = vec![0; BUFFER_SIZE];
+    loop {
+        let num_read = buf_reader.read(&mut buffer)?;
+        if num_read == 0 {
+            break;
+        }
+        buf_writer.write(&buffer[0..num_read])?;
+        total += num_read;
+        print!("{}\r", total)
+    }
+    Ok(())
+}
+
+fn do_transfer(request: &TransferRequest) -> io::Result<()> {
     for source in &request.sources {
-        println!("{} -> {}", source, request.destination)
+        copy(source, request.destination)?
     }
     Ok(())
 }
 
 pub fn run(args: Vec<String>) -> Result<(), String> {
     let request = TransferRequest::new(&args)?;
-    do_transfer(&request)
+    let transfer_outcome = do_transfer(&request);
+    match transfer_outcome {
+        Err(err) => return Err(err.to_string()),
+        Ok(_) => return Ok(())
+    }
 }
 
 #[cfg(test)]
 mod test {
     use super::*;
+    use tempdir::TempDir;
 
     #[test]
     fn parse_sources_and_dest() {
@@ -41,6 +76,30 @@ mod test {
         let transfer_request = TransferRequest::new(&input).expect("invalid input");
         assert_eq!(transfer_request.sources, vec!["src.txt"]);
         assert_eq!(transfer_request.destination, "dest.txt");
+    }
+
+    #[test]
+    fn can_copy_binary_files() {
+        let tmp_dir = TempDir::new("test-rucp").expect("failed to create temp dir");
+        let tmp_dir = tmp_dir.path();
+        let src_path = tmp_dir.join("src.txt");
+        let src_path_name = src_path.to_str().expect("encoding issue");
+        let dest_path = tmp_dir.join("dest.txt");
+        let dest_path_name = dest_path.to_str().expect("encoding issue");
+        let mut src_file = File::create(&src_path).expect("failed to create file");
+        let contents = vec![0xba; 10 * BUFFER_SIZE + 123];
+        src_file.write_all(&contents).expect("could no write to test file");
+        let request = TransferRequest{
+            sources: vec![src_path_name],
+            destination: dest_path_name,
+        };
+        do_transfer(&request).expect("should have worked");
+        let mut dest_file = File::open(&dest_path).expect("failed to open dest");
+        let dest_size :usize = std::fs::metadata(&dest_path).expect("failed to stat dest").len() as usize;
+        assert_eq!(dest_size, contents.len());
+        let mut actual = vec![0; dest_size];
+        dest_file.read(&mut actual).expect("could not read dest");
+        assert_eq!(actual, contents);
     }
 
 }
